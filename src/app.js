cf401832fe57ee3c2a0e108e753417ba0ab6a546
Anurag <anuragraveendren98@gmail.com>
2024-12-12 22:59:25 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,33 +1,44 @@
 const express = require("express");
 const app = express();
-const connectDB =require("./confic/database")
+const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
-const cors = require("cors"); 
+const cors = require("cors");
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+const PORT = process.env.PORT || 3005; 
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || 'http://localhost:3000', 
+    credentials: true,
+  })
+);
 app.use(cookieParser());
+app.use(express.json());
 
-app.use(express.json())
+const authRouter = require("./router/authRouter");
+const eventRouter = require("./router/eventRouter");
+const profileRouter = require("./router/profileRouter");
 
-
-const authRouter=require("./router/authRouter")
-const eventRouter=require("./router/eventRouter")
-const profileRouter=require("./router/profileRouter")
-
-
-app.use("/",authRouter);
-app.use("/",eventRouter);
-app.use("/",profileRouter);
-
+app.use("/", authRouter);
+app.use("/", eventRouter);
+app.use("/", profileRouter);
 
 connectDB()
   .then(() => {
     console.log("Connected to the database");
-    app.listen(3005, () => {
-      console.log("Server is running on port 3005");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((error) => {
     console.error("Failed to connect to the database:", error);
   });
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "An error occurred", error: err.message });
+});
