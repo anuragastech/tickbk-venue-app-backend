33a9969fd62439c75a610ae64e4120a92c7eb699
Anurag <anuragraveendren98@gmail.com>
2024-12-13 01:11:14 +0530

    updated

--- src/confic/database.js
+++ src/confic/database.js
@@ -1,43 +1,19 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+// Load environment variables from .env file
 dotenv.config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-    process.exit(1); 
-  }
+  await mongoose.connect(process.env.MONGODB_URI);
 };
+// connectDB()
+//   .then(() => {
+//     console.log("Connected to the database");
+
+//   })
+//   .catch((error) => {
+//     console.error("Failed to connect to the database:", error);
+//   });
 
 module.exports = connectDB;
-
-
-
-
-
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
-
-// // Load environment variables from .env file
-// dotenv.config();
-
-// const connectDB = async () => {
-//   await mongoose.connect(process.env.MONGODB_URI);
-// };
-// // connectDB()
-// //   .then(() => {
-// //     console.log("Connected to the database");
-
-// //   })
-// //   .catch((error) => {
-// //     console.error("Failed to connect to the database:", error);
-// //   });
-
-// module.exports = connectDB;
